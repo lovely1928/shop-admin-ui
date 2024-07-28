3c598d8ade79dc1f4deb993dbf5afb88480b41b2
lovely <azaad1928@gmail.com>
2024-07-28 21:19:33 +0530

    product management init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { UserComponent } from './features/user/user.component';
 import { DashboardComponent } from './features/dashboard/dashboard.component';
 import { OrderComponent } from './features/order/order.component';
+import { ProductComponent } from './features/product/product.component';
 
 export const routes: Routes = [
   { path: 'user', component: UserComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'order', component: OrderComponent },
+  { path: 'product', component: ProductComponent },
 ];
